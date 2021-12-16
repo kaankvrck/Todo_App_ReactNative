3bba72e84452278cf6692a9768e0e070dbb053fd
Kaan KIVIRCIK <kaankvrck@gmail.com>
2021-12-16 03:02:22 +0100

    Update App.js
    Changing for Task.js

--- App.js
+++ App.js
@@ -1,42 +1,49 @@
-import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
+import Task from './components/Task.js';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 
 const App: () => Node = () => {
   return (
-    <SafeAreaView style={styles.SafeArea}>
+    <SafeAreaView style={styles.safeArea}>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View />
+        <View style={styles.sectionContainer}>
+          {/*Main Title for Todo app */}
+          <View style={styles.tasksWrapper}>
+            <Text style={styles.sectionTitle}> What Should I Do Today</Text>
+            <View style={styles.items}>
+              {/*This area for adding tasks*/}
+              <Task text={'Hi'} />
+              <Task text={'Naber'} />
+            </View>
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    flex: 1,
+    backgroundColor: '#E8EAED',
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
     fontWeight: '700',
+    marginBottom: 25,
+    color: 'black',
   },
+  tasksWrapper: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+  },
+  items: {},
 });
 
 export default App;
