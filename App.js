2e6aa1284606706a2467b687b8e12228dd751cd7
Kaan KIVIRCIK <kaankvrck@gmail.com>
2021-12-16 03:35:27 +0100

    App UI Finished

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import type {Node} from 'react';
 import React from 'react';
 import Task from './components/Task.js';
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView';
 
 const App: () => Node = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View style={styles.sectionContainer}>
+      <View style={styles.sectionContainer}>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
           {/*Main Title for Todo app */}
           <View style={styles.tasksWrapper}>
             <Text style={styles.sectionTitle}> What Should I Do Today</Text>
@@ -17,8 +27,22 @@ const App: () => Node = () => {
               <Task text={'Naber'} />
             </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+        {/*Write task for Todo app*/}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.writeWrapper}>
+          <TextInput
+            style={styles.textInput}
+            placeholder={'Write your tasks for today'}
+          />
+          <TouchableOpacity>
+            <View style={styles.addButton}>
+              <Text>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
     </SafeAreaView>
   );
 };
@@ -43,7 +67,33 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingBottom: 10,
   },
-  items: {},
+  writeWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  textInput: {
+    addingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addButton: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
 });
 
 export default App;
